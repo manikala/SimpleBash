92c678c5ae1956556304b103b6aa806189fec95f
Valeriy Manikala <manikala777@yandex.ru>
2023-11-09 13:55:13 +0300

    Made cat and cat grom file

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -5,6 +5,8 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stddef.h> // определяет макросы нул 
+#include <unistd.h> // содержит множество полезных функций для работы с файловой системой, процессами и другими операционными системными вызовами
+#include <fcntl.h> // позволяет  выполнять  различные  команды  над  открытым файловым дескриптором fd
 
     typedef struct options
 {
@@ -32,8 +34,12 @@
         // {"show-nonprinting", 0, NULL, 'v'},
     }; // данная структура поставляется вместе с самой функцией getopt_long из библиотеки
 
-void parser (int argc, char* argv[], options* options);
+int parser (int argc, char* argv[], options* options);
 void reader (char* argv[], options options);
+
+void read_consol_no_arg (int fd);
+void read_file_no_arg (char* argv[]);
+
 
 
 
@@ -41,25 +47,108 @@ int main (int argc, char* argv[]) {
     
     options my_options = {0, 0, 0, 0, 0, 0, 0, 0}; // заполняем структуру флагов нулями 
 
-    parser (argc, argv, &my_options);
-    if (my_options.b != 0) {
-        my_options.n = 0; 
+    int flag = parser (argc, argv, &my_options);
+    
+    printf("\n flag = %d \n", flag);
+    // printf("\n argc = %d \n", argc);
+    
+    
+
+    if (argc == 1) { //flag == 0
+        read_consol_no_arg (STDIN_FILENO);
+        
+
+    } else {//if (argc == 2)
+        read_file_no_arg (argv);
+        
+    }  
+    
+    // else {
+    //     if (my_options.b != 0) {
+    //     my_options.n = 0; 
+    // }
+    // reader (argv, my_options);   
+
+    // }
+
+
+      
+    return 1;
+}
+
+
+void read_consol_no_arg (int fd) {
+    char buf[4096]; //размер страницы одной в памяти 4кб. для записи сюда из файла 
+    //int fd = STDIN_FILENO; // файловый дискрипт равен 1 STDIN_FILENO
+    int byte_read_amount;
+    byte_read_amount = read(fd, buf, 4096); // функция возращает сколько мы прочитали 
+
+    while (byte_read_amount != 1) // byte_read_amount != 1 //byte_read_amount > 0
+    {
+        printf("%.*s", byte_read_amount, buf); // эта комбинация убирает ошибку с нулем в конце строки и выводится корректно 
+        byte_read_amount = read(fd, buf, 4096);
     }
-    reader (argv, my_options);     
 
 }
 
-void parser (int argc, char* argv[], options *options) {
+
+void read_file_no_arg (char* argv[]) {
+    //(void)argc;
+    int fd = open(argv[1], O_RDONLY); // функция открывает файл. O_RDONLY - значит что мы будем только читать из файла
+    //read_consol_no_arg (open(argv[1], O_RDONLY));
+
+    char buf[4096]; //размер страницы одной в памяти 4кб. для записи сюда из файла 
+    //int fd = STDIN_FILENO; // файловый дискрипт равен 1 STDIN_FILENO
+    int byte_read_amount;
+    byte_read_amount = read(fd, buf, 4096); // функция возращает сколько мы прочитали 
+
+    while (byte_read_amount > 0) // byte_read_amount != 1 //byte_read_amount > 0
+    {
+        printf("%.*s", byte_read_amount, buf); // эта комбинация убирает ошибку с нулем в конце строки и выводится корректно 
+        byte_read_amount = read(fd, buf, 4096);
+    }
+
+
+
+
+
+    // FILE *file = fopen(argv[1], "r"); // optind указатель на следующий аргв
+
+    // if (file) {
+
+    //     int current_value; // текущий символ
+
+
+    //     while ((current_value = fgetc(file)) != EOF ) { // fgetc – чтение одного байта из указанного потока данных
+    //         printf("%d", current_value);
+
+    //     }
+    //     fclose(file);
+    // } else {
+    //     printf("File is not found");
+    // }
+
+}
+
+
+
+
+
+
+
+
+
+int parser (int argc, char* argv[], options *options) {
     
     int res; // результат этой функции будет складываться
-    int long_res; // для длинных аргементов будет складываться результат
+    //int long_res; // для длинных аргементов будет складываться результат
     int res_count = 0;
 
     char short_options[] = "benstvTE";
 
 
 
-    while((res = getopt_long(argc, argv, short_options, long_options, &long_res)) != -1) {
+    while((res = getopt_long(argc, argv, short_options, long_options, NULL)) != -1) {
         switch (res) {
             case 'b':
                 options->b = 1;
@@ -102,17 +191,20 @@ void parser (int argc, char* argv[], options *options) {
             
         }
     }
+
+    return res_count;
 }
+
 
 void reader (char* argv[], options options){
 
-    FILE *file = fopen(argv[optind], "R"); // optind указатель на следующий аргв
+    FILE *file = fopen(argv[optind], "r"); // optind указатель на следующий аргв
 
     if (file) {
 
         int current_value; // текущий символ
         int str_count = 0;
-        int empty_count= 0; // счетчик пустых значений
+        //int empty_count= 0; // счетчик пустых значений
         int count = 0;
 
         while ((current_value = fgetc(file)) != EOF ) { // fgetc – чтение одного байта из указанного потока данных
@@ -129,12 +221,7 @@ void reader (char* argv[], options options){
             }
             if (options.e != 0) {
                 if (current_value != '\n'){
-                    if (count == 0) {
-                        printf ("%6d\t", ++str_count);
-                        count = 1;
-                    } else {
-                        count = 0;
-                    }
+                    printf("$");
                 }    
             }
             if (options.n != 0) {
@@ -178,7 +265,7 @@ void reader (char* argv[], options options){
                 }    
             }
         }
-
+        fclose(file);
     } else {
         printf("File is not found");
     }
