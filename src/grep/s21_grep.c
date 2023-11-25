dd2bd1326208f13d948ff15392e97968b9e3e4d7
Lambert Mango <lambertm@ox-v5.msk.21-school.ru>
2023-11-25 20:34:07 +0300

    Finished grep without tests

--- src/grep/s21_grep.c
+++ src/grep/s21_grep.c
@@ -1,43 +1,5 @@
 #include "s21_grep.h"
 
-#define BUFF 10000
-
-#include <getopt.h>  //для сбора параметров пришедших к нам из командной строки
-#include <stdio.h>
-//#include <stdbool.h>
-#include <stdlib.h>
-//#include <stddef.h> // определяет макросы нул
-//#include <unistd.h> // содержит множество полезных функций для работы с
-//файловой системой, процессами и другими операционными системными вызовами
-//#include <fcntl.h> // позволяет  выполнять  различные  команды  над  открытым
-//файловым дескриптором fd
-#include <regex.h>  // регулярные выражения
-#include <string.h>
-
-typedef struct options {
-  int e;  // шаблон
-  int i;
-  int v;
-  int c;
-  int l;
-  int n;
-  int h;
-  int s;
-  int f;
-  int o;
-  int flag_files;      // если файлов больше 1 то = 1
-  char str_arg[BUFF];  // сюда записываем все аргументы, nтого что мы ищем
-  char str_from_file[BUFF];  // записываем сюда считанную инфу с файла
-
-  char str_o[BUFF];  // строка шаблонов для o
-  char file_name[BUFF];
-
-} options;  // наши опции
-
-int parser(int argc, char* argv[], options* options);
-void result(char* argv[], options* options);
-void flag_f(options* options);
-
 int main(int argc, char* argv[]) {
   options my_options = {0};  // заполняем структуру флагов нулями
   parser(argc, argv, &my_options);
@@ -308,6 +270,7 @@ void result(char* argv[], options* options) {
     }
   }
   regfree(&reg);  // освобождает пямять для регулярных выражений
+  // exit(0);
 }
 
 void flag_f(options* options) {
@@ -322,7 +285,6 @@ void flag_f(options* options) {
       strcat(options->str_arg, options->str_from_file);
       strcat(options->str_arg, "|");
     }
+    fclose(file);
   }
-
-  fclose(file);
 }
--- src/grep/s21_grep.h
+++ src/grep/s21_grep.h
@@ -1,4 +1,42 @@
-#ifdef S21_GREP_H
+#ifndef S21_GREP_H
 #define S21_GREP_H
+
+#define BUFF 10000
+
+#include <getopt.h>  //для сбора параметров пришедших к нам из командной строки
+#include <stdio.h>
+//#include <stdbool.h>
+#include <stdlib.h>
+//#include <stddef.h> // определяет макросы нул
+//#include <unistd.h> // содержит множество полезных функций для работы с
+//файловой системой, процессами и другими операционными системными вызовами
+//#include <fcntl.h> // позволяет  выполнять  различные  команды  над  открытым
+//файловым дескриптором fd
+#include <regex.h>  // регулярные выражения
+#include <string.h>
+
+typedef struct options {
+  int e;  // шаблон
+  int i;
+  int v;
+  int c;
+  int l;
+  int n;
+  int h;
+  int s;
+  int f;
+  int o;
+  int flag_files;      // если файлов больше 1 то = 1
+  char str_arg[BUFF];  // сюда записываем все аргументы, nтого что мы ищем
+  char str_from_file[BUFF];  // записываем сюда считанную инфу с файла
+
+  char str_o[BUFF];  // строка шаблонов для o
+  char file_name[BUFF];
+
+} options;  // наши опции
+
+int parser(int argc, char* argv[], options* options);
+void result(char* argv[], options* options);
+void flag_f(options* options);
 
 #endif
